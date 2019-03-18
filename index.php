33d00e4c5d384ff5c0f5b5069fc8a72fca53e3ee
Max Taylor <mtaylor31801@gmail.com>
2019-03-18 20:55:47 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+    require('head.php');
+?>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1>Project Name</h1>
+                </div>
+                <div class="col-sm-6">
+                    <?php require('nav.php') ?>
+                </div>
+            </div>
+            </nav>
+        </div>
+    </header>
+    
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,6 @@
+<nav class="navbar">
+    <ul class="nav ">
+        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+    </ul>
