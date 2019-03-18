11b49635adf74dcddf8b07b566411f851bd611c8
Max Taylor <mtaylor31801@gmail.com>
2019-03-18 22:27:41 +0000

    finished index.php

--- nav.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbar">
-    <ul class="nav ">
-        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-    </ul>
